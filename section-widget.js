ec5f9cc5a9d0c4d713c7ddf26493093bd947aaf5
Alan Han <alanhan3@gmail.com>
2012-02-27 11:24:06 -0800

    first commit

---
+++ section-widget.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(a){
+a(".swt-wrapper").tabs();
+// CHANGES!!
+a(".swt-accordion-wrapper").accordion();
+a(".swt-height-fix").each(function(c,f)
+{
+var d=a(f).css("display");
+var b=a(f).css("display","inline-block").height();
+a(f).css("display",d).height(b)
+}
+
+)});
