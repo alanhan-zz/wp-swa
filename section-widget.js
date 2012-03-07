4e8b0839eadb643b126bc1dacf68b96170c55bf0
Alan Han <alanhan3@gmail.com>
2012-03-07 11:52:20 -0800

    minified js code

--- section-widget.js
+++ section-widget.js
@@ -1,14 +1,1 @@
-jQuery(document).ready(function(a){
-a(".swt-wrapper").tabs();
-// CHANGES!!
-a(".swt-accordion-wrapper").accordion();
-a(".swt-accordion-wrapper-collapsible").accordion({collapsible: true});
-// ***
-a(".swt-height-fix").each(function(c,f)
-{
-var d=a(f).css("display");
-var b=a(f).css("display","inline-block").height();
-a(f).css("display",d).height(b)
-}
-
-)});
+jQuery(document).ready(function(a){if(jQuery(".swt-wrapper").length>0){a(".swt-wrapper").tabs()}else if(jQuery(".swt-accordion-wrapper").length>0){a(".swt-accordion-wrapper").accordion()}else if(jQuery(".swt-accordion-wrapper-collapsible").length>0){a(".swt-accordion-wrapper-collapsible").accordion({collapsible:true})}a(".swt-height-fix").each(function(c,f){var d=a(f).css("display");var b=a(f).css("display","inline-block").height();a(f).css("display",d).height(b)})});
